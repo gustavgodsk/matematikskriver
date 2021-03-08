db1696f13ca06d5c058c0eb26047512ef88865e6
gustavgodsk <80204659+gustavgodsk@users.noreply.github.com>
2021-03-08 17:10:17 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+    <title title="Matematikskriver">Matematikskriver</title>
+    <link rel="stylesheet" href="styles.css?version=1">
+    <link rel="icon" href="includes/logo.png">
+
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script type="text/javascript" src="includes/script.js"></script>
+    <?php include "includes/ordliste.php"; ?>
+
+</head>
+<body onload="document.getElementById('textarea').focus();">
+
+<div class="kodelink">
+    <h1 id="header">Matematikskriver</h1>
+    <span id="linkkode"><a target=”_blank” href="https://github.com/gustavgodsk/matematikskriver.git">Link til kode</a></span>
+</div>
+
+
+    <br>
+    <br> 
+<div class="kommandoliste">
+    <h4>
+        Kommandoliste
+    </h4>
+    <pre>
+        <span id="span1">x<sup>2 </sup>√ ∛ x<sub>1</sub> ƒ π 𝒗</span>
+        <span title="Potens">"^"</span>
+        <span title="Kvadratrod">"_"</span>
+        <span title="Kubikrod">"__"</span>
+        <span title="Lav tekst (subscript)">";"</span>
+        " over "
+        "/"
+        "f"
+        "pi"
+        "vektor"
+
+        <span id="span2">+ - ⋅ ±</span>
+        "-"
+        "+"
+        "*"
+        "+-"
+        "-+"
+        "gange"
+        "minus"
+        "plus"
+
+        <span id="span3">→ ← ↔</span>
+        "->"
+        "<-"
+        "<->"
+        "højrepil"
+        "venstrepil"
+        "pil til højre"
+        "pil til venstre"
+        "pil til begge sider"
+
+        <span id="span4">° ∠ ⟘</span>
+        " grader"
+        "vinkel"
+        "vinkel "
+        "vinkelret"
+        "er vinkelret på"
+        "er vinkelret med"
+
+        <span id="span5">≈ = ≠</span>
+        "ca"
+        " er "
+        "er ca"
+        "er ca."
+        "er cirka"
+        "er lig med"
+        "ikke lig med"
+        "er ikke lig med"
+
+        <span id="span6">< > ≤ ≥</span>
+        "<="
+        ">="
+        "=<"
+        "=>"
+        "mindre end"
+        "større end"
+        "er mindre end"
+        "er større end"
+        "mindre eller lig med"
+        "større eller lig med"
+        "er mindre eller lig med"
+        "er større eller lig med"
+        "mindre end eller lig med"
+        "større end eller lig med"
+        "er mindre end eller lig med"
+        "er større end eller lig med"
+
+
+        <span id="span7">Fejlkilder</span>
+
+            - kan ikke håndtere brøker med rødder - fx "_9/x"
+            - heller ikke brøker med " over " - fx "x over 1/2"
+            - teksten bliver kun omskrevet hvis kommandoen er skrevet korrekt - fx virker "23over8" ikke, her skal i stedet stå "23 over 8"
+
+
+
+    </pre>
+</div>
+
+
+
+<div class="eksempler">
+    <h4>Eksempler</h4>
+</div>
+<img id="glass" src="includes/hovericon.jpg">
+<div class="tooltip">
+    <img class="size_of_img" src="includes/eksempler/eks1.png" alt="Image 1" /><span class="tooltiptext" id="tooltip1">"vinkel A er 90 grader"</span>
+
+    <img class="size_of_img" src="includes/eksempler/eks2.png" alt="Image 2" /><span class="tooltiptext" id="tooltip2">"vektor a = x;1 over y;1"</span>
+    
+    <img class="size_of_img" src="includes/eksempler/eks3.png" alt="Image 3" /><span class="tooltiptext" id="tooltip3">"pi er cirka 3,14"</span>
+
+    <img class="size_of_img" src="includes/eksempler/eks4.png" alt="Image 4" /><span class="tooltiptext" id="tooltip4">"f(x) er ikke lig med ax^3/b"</span>
+
+    <img class="size_of_img" src="includes/eksempler/eks5.png" alt="Image 5" /><span class="tooltiptext" id="tooltip5">"vinkel A er vinkelret på vinkel B"</span>
+
+    <img class="size_of_img2" src="includes/eksempler/eks6.png" alt="Image 6" /><span class="tooltiptext" id="tooltip6">"__27 <-> _9"</span>
+
+</div>
+
